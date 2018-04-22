2eb6bbe1bb9578699adb1d7f96646f5273b588ed
Folkert van Heusden <mail@vanheusden.com>
2018-04-22 21:41:56 +0200

    css instead of tables

--- view.cpp
+++ view.cpp
@@ -33,7 +33,21 @@ std::string dim_str(const int width, const int height)
 
 std::string view::get_html() const
 {
-	std::string out = "<html><body>";
+	std::string col_size, row_size;
+	for(int i=0; i<grid_width; i++) {
+		if (i)
+			col_size += " max-content";
+		else
+			col_size = "max-content";
+	}
+	for(int i=0; i<grid_height; i++) {
+		if (i)
+			row_size += " max-content";
+		else
+			row_size = "max-content";
+	}
+
+	std::string out = myformat("<html><head><style>.grid-container {\ndisplay: grid;\ngrid-template-columns: %s;\ngrid-column-gap:5px;\ngrid-row-gap:5px;grid-template-rows:%s;\n}\n.grid-item { }\n</style></head><body>", col_size.c_str(), row_size.c_str());
 
 	int each_w = -1, each_h = -1;
 	if (width != -1)
@@ -41,19 +55,21 @@ std::string view::get_html() const
 	if (height != -1)
 		each_h = height / grid_height;
 
-	out += myformat("<table%s>", dim_str(width, height).c_str());
+	out += myformat("<div class=\"grid-container\">");
 
 	size_t nr = 0;
 
 	for(int y=0; y<grid_height; y++) {
+		for(int x=0; x<grid_width; x++) {
+			out += myformat("<div class=\"grid-item\"><img src=\"%s\"%s></div>", http_server::mjpeg_stream_url(cfg, sources.at(nr++)).c_str(), dim_str(each_w, each_h).c_str());
 
-		out += myformat("<tr%s>", dim_str(width, height).c_str());
-
-		for(int x=0; x<grid_width; x++)
-			out += myformat("<td><img src=\"%s\"%s></td>", http_server::mjpeg_stream_url(cfg, sources.at(nr++)).c_str(), dim_str(each_w, each_h).c_str());
+			if (nr == sources.size())
+				goto done;
+		}
 	}
 
-	out += "</table>";
+done:
+	out += "</div>";
 	out += "</body></html>";
 
 	return out;
