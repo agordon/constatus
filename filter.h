53c8c06b93e2eb76e150b878866132186e9604bc
Folkert van Heusden <mail@vanheusden.com>
2018-04-24 15:59:42 +0200

    c++ correctness

--- filter.h
+++ filter.h
@@ -9,13 +9,15 @@
 // NULL filter
 class filter
 {
-public:
+protected:
 	filter();
+
+public:
 	virtual ~filter();
 
 	virtual bool uses_in_out() const { return true; }
-	virtual void apply_io(instance_t *const i, const uint64_t ts, const int w, const int h, const uint8_t *const prev, const uint8_t *const in, uint8_t *const out);
-	virtual void apply(instance_t *const i, const uint64_t ts, const int w, const int h, const uint8_t *const prev, uint8_t *const in_out);
+	virtual void apply_io(instance_t *const i, const uint64_t ts, const int w, const int h, const uint8_t *const prev, const uint8_t *const in, uint8_t *const out) = 0;
+	virtual void apply(instance_t *const i, const uint64_t ts, const int w, const int h, const uint8_t *const prev, uint8_t *const in_out) = 0;
 };
 
 void apply_filters(instance_t *const i, const std::vector<filter *> *const filters, const uint8_t *const prev, uint8_t *const work, const uint64_t ts, const int w, const int h);
